9355636ab1e3f70de98411de343cf1b53458814e
Oleg Nesterov <nesteroff94@gmail.com>
2018-02-04 23:04:17 +0700

    Add dependency and correct mapper controller

--- vestnic-parcer-core/src/main/java/controller/XLTController.java
+++ vestnic-parcer-core/src/main/java/controller/XLTController.java
@@ -1,14 +1,6 @@
 package controller;
 
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import java.io.File;
-import java.io.IOException;
-
-import com.mapforce.DoajMappingConsole;
+import com.mapforce.MappingMapTodoajArticles;
 
 public class XLTController {
 
@@ -27,8 +19,78 @@ public class XLTController {
 
     public void parceXLT(String inputFilePath, String outputFilePath, String xltTemplate) throws Exception {
 
-            DoajMappingConsole.parceXLT(inputFilePath, outputFilePath);
-        }
+          	try {           		// Mapping
 
+			MappingMapTodoajArticles mappingMapTodoajArticles = new MappingMapTodoajArticles();
+
+
+			// run mapping
+			//
+			// you have different options to provide mapping input and output:
+			//
+			// files using file names (available for XML, text, and Excel):
+			//   com.altova.io.FileInput(String filename)
+			//   com.altova.io.FileOutput(String filename)
+			//
+			// streams (available for XML, text, and Excel):
+			//   com.altova.io.StreamInput(java.io.InputStream stream)
+			//   com.altova.io.StreamOutput(java.io.OutputStream stream)
+			//
+			// strings (available for XML and text):
+			//   com.altova.io.StringInput(String xmlcontent)
+			//   com.altova.io.StringOutput()	(call getContent() after run() to get StringBuffer with content)
+			//
+			// Java IO reader/writer (available for XML and text):
+			//   com.altova.io.ReaderInput(java.io.Reader reader)
+			//   com.altova.io.WriterOutput(java.io.Writer writer)
+			//
+			// DOM documents (for XML only):
+			//   com.altova.io.DocumentInput(org.w3c.dom.Document document)
+			//   com.altova.io.DocumentOutput(org.w3c.dom.Document document)
+			//
+			// By default, run will close all inputs and outputs. If you do not want this,
+			// call the following function:
+			// MappingMapTodoajArticlesObject.setCloseObjectsAfterRun(false);
+
+			{
+				com.altova.io.Input Name25420429_2017_17_3_unicode2Source = com.altova.io.StreamInput.createInput(inputFilePath);
+				com.altova.io.Output doajArticles2Target = new com.altova.io.FileOutput(outputFilePath);
+
+				mappingMapTodoajArticles.run(
+						Name25420429_2017_17_3_unicode2Source,
+						doajArticles2Target);
+			}
+
+			System.out.println("Finished");
+		}
+		catch (com.altova.UserException ue)
+		{
+			StringBuilder errorMessage = new StringBuilder();
+			errorMessage.append("USER EXCEPTION:");
+			errorMessage.append(System.lineSeparator());
+			errorMessage.append( ue.getMessage() );
+			throw new Exception(errorMessage.toString());
+		}
+		catch (com.altova.AltovaException e)
+		{
+			StringBuilder errorMessage = new StringBuilder();
+			errorMessage.append("ERROR: ");
+			errorMessage.append(System.lineSeparator());
+			errorMessage.append( e.getMessage() );
+			if (e.getInnerException() != null)
+			{
+				errorMessage.append("Inner exception: ");
+				errorMessage.append(System.lineSeparator());
+				errorMessage.append(e.getInnerException().getMessage());
+				if (e.getInnerException().getCause() != null)
+				{
+					errorMessage.append("Cause: ");
+					errorMessage.append(System.lineSeparator());
+					errorMessage.append(e.getInnerException().getCause().getMessage());
+				}
+			}
+			throw new Exception(errorMessage.toString());
+		}
+	}
 
 }
